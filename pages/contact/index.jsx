ee54f3feed6172b316d95ee5c22e0efd5c1656e3
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2023-02-11 01:05:09 +0600

    adil

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -34,6 +34,9 @@ const Contact = () => {
     message: "",
   });
 
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
@@ -41,19 +44,24 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    setIsSubmitting(true);
+
     try {
+      // send email logic
       await emailjs.send(
         "service_0ll26sg",
         "template_972f6fk",
         formData,
         "627NZaV9FgTHDFx8A"
       );
-      alert("Your message has been sent. Thank you!");
+      setShowSuccessMessage(true);
     } catch (error) {
       alert(
         "An error occurred while sending your message. Please try again later."
       );
       console.error(error);
+    } finally {
+      setIsSubmitting(false);
     }
   };
 
@@ -99,67 +107,81 @@ const Contact = () => {
                 not check them out on a good day.
               </p>
             </div>
-            <form onSubmit={handleSubmit}>
-              <div className="relative z-0 mb-6 mt-10 w-full group">
-                <input
-                  type="text"
-                  name="name"
-                  id="floating_first_name"
-                  className="block py-2.5 px-0 w-full text-md text-[#2e2e2eee] bg-transparent border-0 border-b-2 rounded-b-md border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#b70038] peer"
-                  placeholder=" "
-                  value={formData.name}
-                  onChange={handleChange}
-                  required=""
-                />
-                <label
-                  htmlFor="floating_first_name"
-                  className="absolute text-md text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#b70038]  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+
+            {showSuccessMessage ? (
+             
+              <p className="text-gray-500 border-l-4 border-gray-500 w-fit italic pl-2 pr-3 py-1 bg-[#f5f6fa] rounded-r mt-14">
+            Your message has been sent. Thank you!
+          </p>
+            ) : (
+              <form onSubmit={handleSubmit}>
+                <div className="relative z-0 mb-6 mt-10 w-full group">
+                  <input
+                    type="text"
+                    name="name"
+                    id="floating_first_name"
+                    className="block py-2.5 px-0 w-full text-md text-[#2e2e2eee] bg-transparent border-0 border-b-2 rounded-b-md border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#b70038] peer"
+                    placeholder=" "
+                    value={formData.name}
+                    onChange={handleChange}
+                    required=""
+                  />
+                  <label
+                    htmlFor="floating_first_name"
+                    className="absolute text-md text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#b70038]  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  >
+                    Name
+                  </label>
+                </div>
+                <div className="relative z-0 mb-6 w-full group mt-10 md:mt-0">
+                  <input
+                    type="email"
+                    name="email"
+                    className="block py-2.5 px-0 w-full text-md text-[#2e2e2eee] bg-transparent border-0 border-b-2 rounded-b-md border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#b70038] peer"
+                    placeholder=" "
+                    required=""
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
+                  <label
+                    htmlFor="floating_email"
+                    className="absolute text-md text-gray-500  duration-300 transform -translate-y-12 md:-translate-y-6   scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#b70038]  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-[-15px] md:peer-placeholder-shown:translate-y-0  peer-focus:scale-75 peer-focus:-translate-y-12 md:peer-focus:-translate-y-6"
+                  >
+                    If I were You I would enter My Email address here
+                  </label>
+                </div>
+                <div className="relative z-0 mb-6 w-full group">
+                  <textarea
+                    rows="6"
+                    type="text"
+                    name="message"
+                    className="block py-2.5 px-0 text-md w-full text-md text-[#2e2e2eee] bg-transparent border-0 border-b-2 rounded-b-md  appearance-none border-gray-300 focus:outline-none focus:ring-0 focus:border-[#b70038] peer"
+                    placeholder=" "
+                    required=""
+                    value={formData.message}
+                    onChange={handleChange}
+                  />
+                  <label
+                    htmlFor="floating_email"
+                    className="absolute text-md text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#b70038] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  >
+                    Insert <span className="">Letter combination</span> Here
+                  </label>
+                </div>
+                <button
+                  type="submit"
+                  disabled={
+                    isSubmitting ||
+                    !formData.name ||
+                    !formData.email ||
+                    !formData.message
+                  }
+                  className={`text-white bg-[#b70038]  focus:ring-4 focus:ring-[#b70038] font-medium text-sm w-full sm:w-auto px-5 py-2.5 text-center text-[17px] rounded disabled:bg-[#ff3773]`}
                 >
-                  Name
-                </label>
-              </div>
-              <div className="relative z-0 mb-6 w-full group mt-10 md:mt-0">
-                <input
-                  type="email"
-                  name="email"
-                  className="block py-2.5 px-0 w-full text-md text-[#2e2e2eee] bg-transparent border-0 border-b-2 rounded-b-md border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#b70038] peer"
-                  placeholder=" "
-                  required=""
-                  value={formData.email}
-                  onChange={handleChange}
-                />
-                <label
-                  htmlFor="floating_email"
-                  className="absolute text-md text-gray-500  duration-300 transform -translate-y-12 md:-translate-y-6   scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#b70038]  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-[-15px] md:peer-placeholder-shown:translate-y-0  peer-focus:scale-75 peer-focus:-translate-y-12 md:peer-focus:-translate-y-6"
-                >
-                  If I were You I would enter My Email address here
-                </label>
-              </div>
-              <div className="relative z-0 mb-6 w-full group">
-                <textarea
-                  rows="6"
-                  type="text"
-                  name="message"
-                  className="block py-2.5 px-0 text-md w-full text-md text-[#2e2e2eee] bg-transparent border-0 border-b-2 rounded-b-md  appearance-none border-gray-300 focus:outline-none focus:ring-0 focus:border-[#b70038] peer"
-                  placeholder=" "
-                  required=""
-                  value={formData.message}
-                  onChange={handleChange}
-                />
-                <label
-                  htmlFor="floating_email"
-                  className="absolute text-md text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#b70038] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Insert <span className="">Letter combination</span> Here
-                </label>
-              </div>
-              <button
-                type="submit"
-                className="text-white bg-[#b70038]  focus:ring-4 focus:ring-[#b70038] font-medium text-sm w-full sm:w-auto px-5 py-2.5 text-center text-[17px] rounded"
-              >
-                Send Me Your Letter Combinations
-              </button>
-            </form>
+                  Send Me Your Letter Combinations
+                </button>
+              </form>
+            )}
           </div>
         </div>
       </section>
